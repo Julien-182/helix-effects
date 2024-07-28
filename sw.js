140d1cf9b34cfcb6b8a3e77528b00b64bce1c14c
Julien Ducrot <julien@mackie.home>
2024-07-28 19:09:33 +0200

    Update scope for SW

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@ const cacheName = 'v1';
 const registerServiceWorker = async () => {
   if ("serviceWorker" in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register("/sw.js"/*, {
-        scope: "",
-      }*/);
+      const registration = await navigator.serviceWorker.register("/sw.js", {
+        scope: "/helix-effects/",
+      });
       if (registration.installing) {
         console.log("Service worker installing");
       } else if (registration.waiting) {
